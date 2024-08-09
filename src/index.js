2757f477262ea2b1624f144352eb3213212b7cbe
yuan66082 <66082@sina.com>
2024-08-09 21:13:35 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.docker.zzccrc.com": dockerHub,
+  "quay.docker.zzccrc.com": "https://quay.io",
+  "gcr.docker.zzccrc.com": "https://gcr.io",
+  "k8s-gcr.docker.zzccrc.com": "https://k8s.gcr.io",
+  "k8s.docker.zzccrc.com": "https://registry.k8s.io",
+  "ghcr.docker.zzccrc.com": "https://ghcr.io",
+  "cloudsmith.docker.zzccrc.com": "https://docker.cloudsmith.io",
+  "ecr.docker.zzccrc.com": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.docker.zzccrc.com": dockerHub,
 };
 
 function routeByHosts(host) {
